12b88c41d9bec38cb1aa6d0aefceeaf11a4a7bfa
Yuqiong Liu <yuqiong.liu@nwcal-ucs6.nwc.nws.noaa.gov>
2023-01-13 20:56:12 +0000

    cleanup after AGU

--- agu2022/runs.sh
+++
@@ -1,7 +0,0 @@
-nohup: ignoring input
-Submit new run CFE+TOPMODEL_camels_nse
-=============================================
-
-Wait for current ngen run CFE+TOPMODEL_camels_nse to finish ... 
-Current run CFE+TOPMODEL_camels_nse finished; move output files to designated output directory
-All runs completed!
