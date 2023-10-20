566da0a9a1b69901225c1122ce2d9c582073f0ed
yuqiong.liu <yuqiong.liu@noaa.gov>
2023-10-20 21:14:51 +0000

    update

---
+++ NWMv4/identify_donors/check_gpkg.R
@@ -0,0 +1,37 @@
+#
+rm(list=ls())
+
+library(sf)
+library(data.table)
+
+ver1 <- "v2.0pre"
+hucs <- c("12", "17")
+dt_hucs <- data.table()
+files <- list.files(paste0("../../datasets/gpkg_",ver1,"/gages"),pattern=".gpkg",full.names=TRUE)
+
+for (h1 in hucs) {
+
+# read HUC hydrofabric
+if (ver1=="v2.0pre") {
+    huc <- read_sf(paste0("../../datasets/gpkg_",ver1,"/nextgen_",h1,".gpkg"),"divides")
+} else {
+    huc <- st_read(paste0("../../datasets/gpkg_",ver1,"/huc",h1,"/catchment_data.geojson"))
+}
+
+for (f1 in files) {
+    gage1 <- gsub(".gpkg","",basename(f1))
+    message(gage1)
+    sf1 <- read_sf(f1, 'divides')
+    cats <- sf1$divide_id
+    cats1 <- cats[!cats %in% huc$divide_id]
+    if (length(cats1)==0) dt_hucs <- rbind(dt_hucs,data.table(gage=gage1,huc=h1))
+    #if (length(cats1)>0) message(paste0(length(cats1)," out of ", length(cats)," catchments not found"))
+}}
+
+meta <- as.data.table(read.csv("data/Domain_Meta_NWM_v3.0.csv",stringsAsFactors=FALSE))
+meta <- meta[, c("gage_id", "lat", "lon"), with = FALSE]
+meta <- meta[!duplicated(meta$gage_id)]
+meta <- na.omit(meta)
+dt_hucs <- merge(dt_hucs, meta, by.x="gage",by.y="gage_id",all.x=TRUE)
+
+save(dt_hucs, file="data/list_gage_huc.Rdata")
